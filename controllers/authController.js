8d73c901aecf35ecc75409bc519ca83a2b948f59
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-07 11:34:37 +0300

    Change files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const userService = require('../services/userService');
+
 router.get('/auth', (req, res) => {
     res.send('authController');
 });
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.post('/register', (req, res) => {
+
+    let data = req.body;
+
+    userService.createUser(data);
+
+    res.redirect('/');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res) => {
+
+    let data = req.body;
+
+    userService.createUser(data);
+
+    res.redirect('/');
+
+    res.redirect('/');
+});
+
 
 module.exports = router;
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,39 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const userService = require('../services/userService');
+
 
 router.get('/', (req, res) => {
     res.render('home');
 });
 
-router.get('/register', (req, res) => {
-    res.render('register');
-});
-
-router.post('/register', (req, res) => {
-
-    let data = req.body;
-
-    userService.createUser(data);
-
-    res.redirect('/');
-});
-
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
-router.post('/login', (req, res) => {
-
-    let data = req.body;
-
-    userService.createUser(data);
-
-    res.redirect('/');
-
-    res.redirect('/');
-});
 
 router.get('/profile', (req, res) => {
     res.render('profile');
