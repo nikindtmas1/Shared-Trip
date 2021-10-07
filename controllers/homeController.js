a69b2c4180db3087766d42433443f1f2c23b163b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-07 10:59:34 +0300

    Added post logic user

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const userService = require('../services/userService');
+
 router.get('/', (req, res) => {
     res.render('home');
 });
@@ -9,8 +11,28 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.post('/register', (req, res) => {
+
+    let data = req.body;
+
+    userService.createUser(data);
+
+    res.redirect('/');
+});
+
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+router.post('/login', (req, res) => {
+
+    let data = req.body;
+
+    userService.createUser(data);
+
+    res.redirect('/');
+
+    res.redirect('/');
 });
 
 router.get('/profile', (req, res) => {
@@ -24,5 +46,21 @@ router.get('/shared-trips', (req, res) => {
 router.get('/offer-trip', (req, res) => {
     res.render('trip-create');
 });
+
+
+function letValide(req, res, next){
+    let isValid = true;
+
+    if(req.body.email.trim().length < 2){
+        isValid = false;
+    }else if(!req.body.password){
+        isValid = false;
+    }
+
+    if(isValid){
+        next();
+    }
+
+}
 
 module.exports = router;
