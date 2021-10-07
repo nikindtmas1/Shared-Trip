07c2a1ea43eec579ed343143d33caa6fdc58174e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-07 10:59:07 +0300

    Change model

--- models/user.js
+++ models/user.js
@@ -9,13 +9,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    gender: {
-        type: String,
-        required: true
-    },
-    tripsHistory: {
-        
-    }
+    
 });
 
 module.exports = mongoose.model('User', userSchema);
