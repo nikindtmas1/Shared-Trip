4d5a4ffaa464eb1ac4d834179014960f3953958f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-07 09:57:12 +0300

    Change model user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String
+    email: {
+        type: String,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    tripsHistory: {
+        
     }
 });
 
