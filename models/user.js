8af097028d8d7d16eb3b0f76bd366e56404073e7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-13 14:45:36 +0300

    Added gender in schema

--- models/user.js
+++ models/user.js
@@ -13,7 +13,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-   
+   gender: {
+       type: String,
+       required: true
+   },
     history: 
         [{
             type: 'ObjectId',
