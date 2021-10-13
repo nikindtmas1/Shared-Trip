425e130e10893e5a30a8df089f9b24a1b2d57919
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-13 11:04:29 +0300

    Change function ferify token

--- middlewares/authMIddleware.js
+++ middlewares/authMIddleware.js
@@ -1,18 +1,40 @@
 const jwt = require('jsonwebtoken');
 const { SICRET } = require('../config/config');
 
-function verifyToken(token) {
+// function verifyToken(token) {
 
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, SICRET, (err, payload) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(payload);
-        })
-    })
-};
+//     return new Promise((resolve, reject) => {
+//         jwt.verify(token, SICRET, (err, payload) => {
+//             if (err) {
+//                 reject(err);
+//                 return;
+//             }
+//             resolve(payload);
+//         })
+//     })
+// };
+
+function auth(req, res, next){
+    let token = req.cookies.cookieToken;
+  
+
+    if(!token){
+        return next();
+    }
+
+    jwt.verify(token, SICRET, function(err, decodedToken){
+        if(err) {
+            return res.status(401).redirect('/auth/login');
+        }
+
+        //let email = req.body.email;
+        req.user = decodedToken;
+        res.locals.user = decodedToken;
+        //res.locals.email = req.body.email;
+        next();
+    });
+
+}
 
 function isAuth(req, res, next){
     if(!req.user){
@@ -23,6 +45,7 @@ function isAuth(req, res, next){
 }
 
 module.exports = {
-    verifyToken,
+    //verifyToken,
+    auth,
     isAuth
 }
